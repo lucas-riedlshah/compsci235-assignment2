af95cd90769be9f630ef97abf83f68511e205719
Lucas RiedlShah <lucasp.riedlshah@gmail.com>
2021-09-17 01:27:55 +1200

    created books service functions and tests

---
+++ library/books/services.py
@@ -0,0 +1,14 @@
+from library.adapters.repository import AbstractRepository
+from library.domain import Book
+from math import ceil
+
+BOOKS_PER_PAGE = 8
+
+def get_book(repo: AbstractRepository):
+    pass
+
+def get_nth_books_page(repo: AbstractRepository, page: int) -> list[Book]:
+    return repo.get_all_books()[page * BOOKS_PER_PAGE - BOOKS_PER_PAGE: page * BOOKS_PER_PAGE]
+
+def get_books_page_count(repo: AbstractRepository):
+    return ceil(len(repo.get_all_books()) / BOOKS_PER_PAGE)
