f1b7340d6293537f9e46faf38da1176f5546577b
Lucas RiedlShah <lucasp.riedlshah@gmail.com>
2021-09-16 19:51:28 +1200

    configured app setup, added home blueprint

--- library/__init__.py
+++ library/__init__.py
@@ -1,32 +1,29 @@
 """Initialize Flask app."""
 
+from pathlib import Path
+
 from flask import Flask, render_template
 
-# TODO: Access to the books should be implemented via the repository pattern and using blueprints, so this can not stay here!
-from library.domain import Book
-
-# TODO: Access to the books should be implemented via the repository pattern and using blueprints, so this can not stay here!
-def create_some_book():
-    some_book = Book(1, "Harry Potter and the Chamber of Secrets")
-    some_book.description = "Ever since Harry Potter had come home for the summer, the Dursleys had been so mean \
-                             and hideous that all Harry wanted was to get back to the Hogwarts School for \
-                             Witchcraft and Wizardry. But just as he’s packing his bags, Harry receives a \
-                             warning from a strange impish creature who says that if Harry returns to Hogwarts, \
-                             disaster will strike."
-    some_book.release_year = 1999
-    return some_book
+import library.adapters.repository as repository
+from library.adapters.repository import populate
+from library.adapters.memory_repository import MemoryRepository
 
 
-def create_app():
+def create_app(test_config = None):
     app = Flask(__name__)
 
-    # @app.route('/')
-    # def home():
-    #     some_book = create_some_book()
-    #     # Use Jinja to customize a predefined html page rendering the layout for showing a single book.
-    #     return render_template('simple_book.html', book=some_book)
-    @app.route('/')
-    def home():
-        return render_template('home/home.html')
+    app.config.from_object('config.Config')
+    data_path = Path('library') / 'adapters' / 'data'
+
+    if test_config is not None:
+        app.config.from_mapping(test_config)
+        data_path = app.config['TEST_DATA_PATH']
+
+    repository.repo_instance = MemoryRepository()
+    populate(data_path, repository.repo_instance)
+
+    with app.app_context():
+        from .home import home
+        app.register_blueprint(home.home_blueprint)
 
     return app
