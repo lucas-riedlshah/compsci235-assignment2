9d67ca940f516ae4f9cf8e62237f2d6eaea253b2
Lucas RiedlShah <lucasp.riedlshah@gmail.com>
2021-09-18 03:09:52 +1200

    added fallback for sort if release_year None
    will work for... the foreseeable future. Until the year 99999 passes...

--- library/authors/services.py
+++ library/authors/services.py
@@ -5,18 +5,25 @@ from math import ceil
 AUTHORS_PER_PAGE = 25
 AUTHOR_BOOKS_PER_PAGE = 10
 
+
 def get_author(repo: AbstractRepository, author_id: int):
     return repo.get_author(author_id)
 
+
 def get_nth_books_by_author_page(repo: AbstractRepository, author_id: int, page: int):
-    books = sorted(repo.get_books_by_author(get_author(repo, author_id)), key=lambda book: book.release_year, reverse=True)
+    """Gets the nth books-by-author page"""
+    books = sorted(repo.get_books_by_author(get_author(repo, author_id)),
+                   key=lambda book: book.release_year if book.release_year is not None else 99999, reverse=True)
     return books[page * AUTHOR_BOOKS_PER_PAGE - AUTHOR_BOOKS_PER_PAGE: page * AUTHOR_BOOKS_PER_PAGE]
+
 
 def get_books_by_author_page_count(repo: AbstractRepository, author_id: int):
     return ceil(len(repo.get_books_by_author(get_author(repo, author_id))) / AUTHOR_BOOKS_PER_PAGE)
 
+
 def get_nth_authors_page(repo: AbstractRepository, page: int) -> list[Author]:
     return sorted(repo.get_all_authors(), key=lambda author: author.full_name)[page * AUTHORS_PER_PAGE - AUTHORS_PER_PAGE: page * AUTHORS_PER_PAGE]
+
 
 def get_authors_page_count(repo: AbstractRepository):
     return ceil(len(repo.get_all_authors()) / AUTHORS_PER_PAGE)
