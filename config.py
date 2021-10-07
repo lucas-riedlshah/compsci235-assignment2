ac7a427af975b16cc4ad3f63de81038fd422d637
Lucas RiedlShah <lucasp.riedlshah@gmail.com>
2021-10-07 15:45:14 +1300

    added new sqlalchemy env vars

--- config.py
+++ config.py
@@ -15,3 +15,8 @@ class Config:
     SECRET_KEY = environ.get('SECRET_KEY')
 
     TESTING = environ.get('TESTING')
+
+    REPOSITORY = environ.get('REPOSITORY')
+
+    SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_ECHO = environ.get('SQLALCHEMY_ECHO')
