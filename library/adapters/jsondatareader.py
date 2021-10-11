30fac555253007f95fda6f62e99f3898ac18b253
Lucas RiedlShah <lucasp.riedlshah@gmail.com>
2021-10-11 16:27:49 +1300

    changed to ensure only 1 instance of a given Publisher or Author

--- library/adapters/jsondatareader.py
+++ library/adapters/jsondatareader.py
@@ -35,10 +35,12 @@ class BooksJSONReader:
         authors_json = self.read_authors_file()
         books_json = self.read_books_file()
 
+        publishers = {}
+        authors = {}
+
         for book_json in books_json:
             book_instance = Book(int(book_json['book_id']), book_json['title'])
             book_instance.isbn = book_json['isbn']
-            book_instance.publisher = Publisher(book_json['publisher'])
             if book_json['publication_year'] != "":
                 book_instance.release_year = int(book_json['publication_year'])
             if book_json['is_ebook'].lower() == 'false':
@@ -49,26 +51,35 @@ class BooksJSONReader:
             book_instance.description = book_json['description']
             if book_json['num_pages'] != "":
                 book_instance.num_pages = int(book_json['num_pages'])
+            
+            publisher_name = book_json['publisher']
+            if publisher_name not in publishers:
+                publishers[publisher_name] = Publisher(publisher_name)
+            book_instance.publisher = publishers[publisher_name]
 
             # extract the author ids:
             list_of_authors_ids = book_json['authors']
             for author_id in list_of_authors_ids:
 
                 numerical_id = int(author_id['author_id'])
-                # We assume book authors are available in the authors file,
-                # otherwise more complex handling is required.
-                author_name = None
-                author_average_rating = None
-                author_ratings_count = None
-                for author_json in authors_json:
-                    if int(author_json['author_id']) == numerical_id:
-                        author_name = author_json['name']
-                        author_average_rating = float(author_json['average_rating'])
-                        author_ratings_count = int(author_json['ratings_count'])
-                author = Author(numerical_id, author_name)
-                author.average_rating = author_average_rating
-                author.ratings_count = author_ratings_count
-                book_instance.add_author(author)
+
+                if numerical_id not in authors:
+                    # We assume book authors are available in the authors file,
+                    # otherwise more complex handling is required.
+                    author_name = None
+                    author_average_rating = None
+                    author_ratings_count = None
+                    for author_json in authors_json:
+                        if int(author_json['author_id']) == numerical_id:
+                            author_name = author_json['name']
+                            author_average_rating = float(author_json['average_rating'])
+                            author_ratings_count = int(author_json['ratings_count'])
+                    author = Author(numerical_id, author_name)
+                    author.average_rating = author_average_rating
+                    author.ratings_count = author_ratings_count
+                    authors[numerical_id] = author
+
+                book_instance.add_author(authors[numerical_id])
 
 
             self.__dataset_of_books.append(book_instance)
