8c013e9ed18801bac0ceb5d0106985f91803525a
Lucas RiedlShah <lucasp.riedlshah@gmail.com>
2021-10-16 17:34:51 +1300

    added sqlalchemy database support

--- library/__init__.py
+++ library/__init__.py
@@ -3,13 +3,18 @@
 from pathlib import Path
 
 from flask import Flask, render_template
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, clear_mappers
+from sqlalchemy.pool import NullPool
 
 import library.adapters.repository as repository
 from library.adapters.repository import populate
 from library.adapters.memory_repository import MemoryRepository
+from library.adapters.database_repository import SqlAlchemyRepository
+from library.adapters.orm import metadata, map_model_to_tables
 
 
-def create_app(test_config = None):
+def create_app(test_config=None):
     app = Flask(__name__)
 
     app.config.from_object('config.Config')
@@ -19,8 +24,36 @@ def create_app(test_config = None):
         app.config.from_mapping(test_config)
         data_path = app.config['TEST_DATA_PATH']
 
-    repository.repo_instance = MemoryRepository()
-    populate(data_path, repository.repo_instance)
+    if app.config['REPOSITORY'] == 'memory':
+        repository.repo_instance = MemoryRepository()
+        populate(data_path, repository.repo_instance)
+    elif app.config['REPOSITORY'] == 'database':
+        database_uri = app.config['SQLALCHEMY_DATABASE_URI']
+        database_echo = app.config['SQLALCHEMY_ECHO']
+
+        database_engine = create_engine(
+            database_uri,
+            connect_args={'check_same_thread': False},
+            poolclass=NullPool,
+            echo=database_echo
+        )
+
+        session_factory = sessionmaker(autocommit=False, autoflush=True, bind=database_engine)
+
+        repository.repo_instance = SqlAlchemyRepository(session_factory)
+        
+        if app.config['TESTING'] == 'True' or len(database_engine.table_names()) == 0:
+            clear_mappers()
+
+            metadata.create_all(database_engine)
+            for table in reversed(metadata.sorted_tables):
+                database_engine.execute(table.delete())
+
+            map_model_to_tables()
+
+            populate(data_path, repository.repo_instance, database_mode=True)
+        else:
+            map_model_to_tables()
 
     with app.app_context():
         from .home import home
@@ -40,5 +73,15 @@ def create_app(test_config = None):
 
         from .authentication import authentication
         app.register_blueprint(authentication.authentication_blueprint)
+
+        @app.before_request
+        def before_http_request():
+            if isinstance(repository.repo_instance, SqlAlchemyRepository):
+                repository.repo_instance.reset_session()
+            
+        @app.teardown_appcontext
+        def shutdown_session(exception=None):
+            if isinstance(repository.repo_instance, SqlAlchemyRepository):
+                repository.repo_instance.close_session()
 
     return app
