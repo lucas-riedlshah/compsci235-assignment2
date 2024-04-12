254095a26b5c2c65eb4292f7db050a816b0df413
Lucas RiedlShah <lucasp.riedlshah@gmail.com>
2024-04-12 20:14:32 +1200

    Change home page to show recommended books instead of recent books
    The recent ones don't have images available

--- library/home/home.py
+++ library/home/home.py
@@ -5,8 +5,9 @@ from library.adapters.repository import repo_instance as repo
 
 home_blueprint = Blueprint('home_bp', __name__)
 
+recommended_books = [12898900, 2687401, 1348630, 11827783]
 
 @home_blueprint.route('/')  # , methods=['GET'])
 def home():
-    recent_books = get_recent_books(repo)
+    recent_books = get_recommended_books(repo, recommended_books)
     return render_template('home/home.html', books=recent_books)
--- library/home/services.py
+++ library/home/services.py
@@ -1,6 +1,8 @@
 from library.adapters.repository import AbstractRepository
 from library.utilities import book_to_dict
 
-def get_recent_books(repo: AbstractRepository):
-    books = sorted(repo.get_all_books(), key=lambda book: book.release_year or 0, reverse=True)[:4]
+def get_recommended_books(repo: AbstractRepository, book_ids: list[int]):
+    books = []
+    for id in book_ids:
+        books.append(book_to_dict(repo.get_book(id)))
     return books
